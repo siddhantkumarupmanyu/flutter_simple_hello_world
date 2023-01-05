0b7827490b1bf9aa6cf18bf21bf99d5e2d5f39b8
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-05 18:22:23 +0530

    move to Screen1.dart

---
+++ lib/Screen1.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'MyScaffold.dart';
+
+class Screen1 extends ConsumerWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    _runsAfterBuild(ref);
+
+    ref.watch(myControllerProvider);
+
+    return const MaterialApp(
+      home: MyScaffold(),
+    );
+  }
+
+  // https://stackoverflow.com/a/72286340
+  Future<void> _runsAfterBuild(WidgetRef ref) async {
+    await Future(() {}); // <-- Dummy await
+    print("build complete");
+    ref.read(myControllerProvider).initialize();
+  }
+}
+
+final myControllerProvider =
+Provider.autoDispose((ref) => Screen1Controller(ref));
+
+class Screen1Controller {
+  final ProviderRef<Screen1Controller> ref;
+  int count = 0;
+
+  Screen1Controller(this.ref);
+
+  void initialize() {
+    ref.read(onPressedProvider.notifier).state = myAppControllerOnPressed;
+  }
+
+  void myAppControllerOnPressed() {
+    print("appAppController Override onPressed");
+    ref.read(textProvider.notifier).state =
+    "set from myAppController ${count++}";
+  }
+}
