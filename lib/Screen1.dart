af4c4b8dc7fe001b29c15947740b4b5f9b937061
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-05 18:51:37 +0530

    it did navigate but why 2nd screens controller not working

--- lib/Screen1.dart
+++ lib/Screen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'MyScaffold.dart';
 
@@ -11,6 +12,10 @@ class Screen1 extends ConsumerWidget {
     _runsAfterBuild(ref);
 
     ref.watch(myControllerProvider);
+
+    ref.listen<String>(screen1Navigator, (String? oldState, String newState) {
+      context.push(newState);
+    });
 
     return const MyScaffold();
   }
@@ -38,7 +43,13 @@ class Screen1Controller {
 
   void myAppControllerOnPressed() {
     print("appAppController Override onPressed");
+    if (count > 10) {
+      ref.read(screen1Navigator.notifier).state = "/screen2";
+    }
     ref.read(textProvider.notifier).state =
         "set from myAppController ${count++}";
   }
 }
+
+// can we have single navigator for both screen, like global navigator???
+final screen1Navigator = StateProvider((ref) => "/nothing");
