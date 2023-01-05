fff22bdd3ba7be0c9d3cf3e5d49f4e3cef19960e
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-05 17:02:15 +0530

    hello riverpod from flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final helloWorldProvider = Provider((ref) => "Hello Riverpod from Provider");
 
 void main() {
-  // runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello RiverPod")),
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
+  }
 }
