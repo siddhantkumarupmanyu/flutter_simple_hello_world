ecdecf95189b1ada1ba9994935184619d2a2abf7
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-05 17:21:44 +0530

    externalize onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final helloWorldProvider = StateProvider((ref) => "Hello Riverpod from Provider");
+final textProvider = StateProvider((ref) => "Hello Riverpod from Provider");
+
+final onPressedProvider = StateProvider((ref) {
+  return () {
+    print("onPressed default");
+  };
+});
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -12,19 +18,18 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final value = ref.watch(helloWorldProvider);
+    final textValue = ref.watch(textProvider);
+    final onPressedValue = ref.watch(onPressedProvider);
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Hello RiverPod")),
         body: Center(
-          child: Text(value),
+          child: Text(textValue),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: onPressedValue,
           child: const Icon(Icons.add),
-          onPressed: () {
-            ref.read(helloWorldProvider.notifier).update((state) => " $state : addition");
-          },
         ),
       ),
     );
