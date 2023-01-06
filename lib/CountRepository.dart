68e4db1787bb397035f311b6993cac584d937179
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-07 04:27:58 +0530

    when first listener is added it emits the last value

--- lib/CountRepository.dart
+++ lib/CountRepository.dart
@@ -1,18 +1,30 @@
+import 'dart:async';
 
-
-class CountRepository{
-
+class CountRepository {
   int _count = 0;
 
+  final StreamController<int> _countStreamController = StreamController();
 
+  Stream<int> get countSteam => _countStreamController.stream;
 
+  final int _saveDuration;
+
+  CountRepository(this._saveDuration) {
+    _countStreamController.onListen = (){
+      _countStreamController.add(_count);
+    };
+  }
+
+  // todo: make this return future
   int getCount() {
     return _count;
   }
 
-  Future<void> saveCount(int count) async{
-    await Future.delayed(const Duration(seconds: 2));
+  Future<void> saveCount(int count) async {
+    // await Future.delayed(Duration(milliseconds: _saveDuration));
     _count = count;
+    if (_countStreamController.hasListener) {
+      _countStreamController.add(_count);
+    }
   }
-
 }
--- test/CountRepositoryTest.dart
+++ test/CountRepositoryTest.dart
@@ -3,10 +3,20 @@ import 'package:simple_hello_world/CountRepository.dart';
 
 void main() {
   test("savesCount", () async {
-    final repo = CountRepository();
+    final repo = CountRepository(100);
 
     await repo.saveCount(10);
 
     expect(repo.getCount(), equals(10));
+  });
+
+  test("emitsCount", () async {
+    final repo = CountRepository(100);
+
+    repo.saveCount(10);
+
+    expect(repo.countSteam, emitsInOrder([10, 20]));
+
+    repo.saveCount(20);
   });
 }
