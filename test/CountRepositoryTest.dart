054b7d1952601f4eb6b738f4306e9b80547af397
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-07 04:31:58 +0530

    emitsCount

--- test/CountRepositoryTest.dart
+++ test/CountRepositoryTest.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_hello_world/CountRepository.dart';
 
+import 'package:async/async.dart';
+
+
 void main() {
   test("savesCount", () async {
     final repo = CountRepository(100);
@@ -12,11 +15,13 @@ void main() {
 
   test("emitsCount", () async {
     final repo = CountRepository(100);
+    final streamQueue = StreamQueue<int>(repo.countSteam);
 
+    // not awaiting for it to save
     repo.saveCount(10);
-
-    expect(repo.countSteam, emitsInOrder([10, 20]));
+    expect(await streamQueue.next, equals(10));
 
     repo.saveCount(20);
+    expect(await streamQueue.next, equals(20));
   });
 }
