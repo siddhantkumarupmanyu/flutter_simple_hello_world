3ac96234462e97c0a7875ef9a9bbf6f5e5d26161
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-06 18:22:59 +0530

    next steps...

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:go_router/go_router.dart';
 import 'Screen1.dart';
 import 'Screen2.dart';
 
+// next step
+// - add a repo or some global stuff in main() and let it be consumed in GoRoute builder,
+//    so no need to have/take anything in MyApp
+// - add tests.
+// - if possible take one step forward by creating and reusing same widget in different contexts.
+
 void main() {
   runApp(const MyApp());
 }
