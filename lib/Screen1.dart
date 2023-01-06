faafd1a1d77fe9d32c8f9293ae04fc14c3231c97
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-06 13:45:05 +0530

    make not needed things private

--- lib/Screen1.dart
+++ lib/Screen1.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -5,54 +7,47 @@ import 'package:provider/provider.dart';
 import 'MyScaffold.dart';
 
 class Screen1 extends StatelessWidget {
-  late final Screen1Vm screenVm;
+  late final Screen1Vm _screenVm;
 
-  late BuildContext buildContext;
+  late BuildContext _buildContext;
 
   Screen1({super.key}){
-    this.screenVm = Screen1Vm(this._navigateTo);
+    this._screenVm = Screen1Vm(this._navigateTo);
   }
 
   @override
   Widget build(BuildContext context) {
-    buildContext = context;
+    _buildContext = context;
     return MultiProvider(
       providers: [
         StreamProvider<MyScaffoldValue>.value(
-          value: screenVm.testSteam,
+          value: _screenVm.myScaffoldValueStream,
           initialData: const MyScaffoldValue("100"),
         ),
         Provider<MyScaffoldOnPressed>.value(
-            value: MyScaffoldOnPressed(screenVm.onPressed)),
+            value: MyScaffoldOnPressed(_screenVm.onPressed)),
       ],
       child: const MyScaffold(),
     );
   }
 
   void _navigateTo() {
-    buildContext.push("/screen2");
+    _buildContext.push("/screen2");
   }
 }
 
 class Screen1Vm {
-  final void Function() navigateTo;
+  final void Function() _navigateTo;
 
-  Stream<MyScaffoldValue> get testSteam async* {
-    var i = 0;
-    while (i < 85) {
-      await Future.delayed(const Duration(seconds: 2), () {
-        i++;
-      });
-      yield MyScaffoldValue(i.toString());
-    }
-  }
+  final StreamController<MyScaffoldValue> _myScaffoldValueStreamController = StreamController();
+
+  Stream<MyScaffoldValue> get myScaffoldValueStream => _myScaffoldValueStreamController.stream;
 
   int _count = 0;
 
-  Screen1Vm(this.navigateTo);
+  Screen1Vm(this._navigateTo);
 
   void onPressed() {
-    // todo:
     // _count++;
     // sv.setText("screenVm1: $count");
     // if (count > 10) {
