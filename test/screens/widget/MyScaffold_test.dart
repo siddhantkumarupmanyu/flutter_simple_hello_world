f36279493e1c8d14fa687c5d9e5e8c66b694aa4a
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-26 12:34:13 +0530

    MyScaffold watchesForValueChange

--- test/screens/widget/MyScaffold_test.dart
+++ test/screens/widget/MyScaffold_test.dart
@@ -1,15 +1,21 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_hello_world/widgets/MyScaffold.dart';
 
 void main() {
-  testWidgets("learningTest", (widgetTester) async {
+
+  testWidgets("watchesForValueChange", (widgetTester) async {
+    final streamController = StreamController<MyScaffoldValue>();
+
     await widgetTester.pumpWidget(MaterialApp(
         home: MultiProvider(
           providers: [
-            Provider<MyScaffoldValue>.value(
-              value: const MyScaffoldValue("100"),
+            StreamProvider<MyScaffoldValue>.value(
+              value: streamController.stream,
+              initialData: const MyScaffoldValue("100"),
             ),
             Provider<MyScaffoldOnPressed>.value(
                 value: MyScaffoldOnPressed((){})),
@@ -19,8 +25,13 @@ void main() {
     ));
 
     expect(find.text("Hello Provider"), findsOneWidget);
+    expect(find.text("100"), findsOneWidget);
 
-    final test = find.text("100");
-    expect(test, findsOneWidget);
+    streamController.add(const MyScaffoldValue("20"));
+
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text("20"), findsOneWidget);
   });
+
 }
