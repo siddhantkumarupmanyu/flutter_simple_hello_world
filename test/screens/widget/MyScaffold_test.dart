de60f724192a44c67da644891242a9fdca848afd
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-26 12:43:15 +0530

    MyScaffold callsOnPressed

--- test/screens/widget/MyScaffold_test.dart
+++ test/screens/widget/MyScaffold_test.dart
@@ -6,23 +6,20 @@ import 'package:provider/provider.dart';
 import 'package:simple_hello_world/widgets/MyScaffold.dart';
 
 void main() {
-
   testWidgets("watchesForValueChange", (widgetTester) async {
     final streamController = StreamController<MyScaffoldValue>();
 
     await widgetTester.pumpWidget(MaterialApp(
         home: MultiProvider(
-          providers: [
-            StreamProvider<MyScaffoldValue>.value(
-              value: streamController.stream,
-              initialData: const MyScaffoldValue("100"),
-            ),
-            Provider<MyScaffoldOnPressed>.value(
-                value: MyScaffoldOnPressed((){})),
-          ],
-          child: const MyScaffold(),
-        )
-    ));
+      providers: [
+        StreamProvider<MyScaffoldValue>.value(
+          value: streamController.stream,
+          initialData: const MyScaffoldValue("100"),
+        ),
+        Provider<MyScaffoldOnPressed>.value(value: MyScaffoldOnPressed(() {})),
+      ],
+      child: const MyScaffold(),
+    )));
 
     expect(find.text("Hello Provider"), findsOneWidget);
     expect(find.text("100"), findsOneWidget);
@@ -34,4 +31,29 @@ void main() {
     expect(find.text("20"), findsOneWidget);
   });
 
+  testWidgets("callsOnPressed", (widgetTester) async {
+    var isCalled = false;
+
+    await widgetTester.pumpWidget(MaterialApp(
+        home: MultiProvider(
+      providers: [
+        Provider<MyScaffoldValue>.value(
+          value: const MyScaffoldValue("100"),
+        ),
+        Provider<MyScaffoldOnPressed>.value(value: MyScaffoldOnPressed(() {
+          isCalled = true;
+        })),
+      ],
+      child: const MyScaffold(),
+    )));
+
+    expect(find.text("Hello Provider"), findsOneWidget);
+    expect(find.text("100"), findsOneWidget);
+
+    await widgetTester.tap(find.byType(FloatingActionButton));
+
+    await widgetTester.pumpAndSettle();
+
+    expect(isCalled, equals(true));
+  });
 }
