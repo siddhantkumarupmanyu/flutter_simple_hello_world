77fd9de8f9da81ff60e54d2d1062f2dab8e83a58
siddhant <siddhantkumarupmanyu@gmail.com>
2023-01-26 12:25:16 +0530

    learningTest passing

--- test/screens/widget/MyScaffold_test.dart
+++ test/screens/widget/MyScaffold_test.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_hello_world/widgets/MyScaffold.dart';
 
 void main() {
   testWidgets("learningTest", (widgetTester) async {
-    await widgetTester.pumpWidget(const MaterialApp(
-        body: Center(
-          child: Text("test"),
-    )));
+    await widgetTester.pumpWidget(MaterialApp(
+        home: MultiProvider(
+          providers: [
+            Provider<MyScaffoldValue>.value(
+              value: const MyScaffoldValue("100"),
+            ),
+            Provider<MyScaffoldOnPressed>.value(
+                value: MyScaffoldOnPressed((){})),
+          ],
+          child: const MyScaffold(),
+        )
+    ));
 
-    final test = find.text("test");
+    expect(find.text("Hello Provider"), findsOneWidget);
 
+    final test = find.text("100");
     expect(test, findsOneWidget);
   });
 }
