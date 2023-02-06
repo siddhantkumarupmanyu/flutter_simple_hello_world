b0e4d7cd7cbaf08564748d637ceeeb56fda2b387
siddhant <siddhantkumarupmanyu@gmail.com>
2023-02-06 19:25:23 +0530

    router in doing the dependency injection for now.

--- integration_test/e2e_test.dart
+++ integration_test/e2e_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:simple_hello_world/main.dart' as app;
@@ -12,6 +13,6 @@ void main() {
     expect(find.text("Screen 1"), findsOneWidget);
     expect(find.text("0"), findsOneWidget);
 
-    // await tester.tap(find.byType(FloatingActionButton));
+    await tester.tap(find.byType(FloatingActionButton));
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:simple_hello_world/CountRepository.dart';
+import 'package:simple_hello_world/InMemoryCountRepository.dart';
+import 'package:simple_hello_world/screens/screen_1/Screen1Vm.dart';
 
 import 'screens/screen_1/Screen1.dart';
 import 'screens/screen_2/Screen2.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
 
 GoRouter router() {
+  CountRepository countRepo = InMemoryCountRepository(200);
+
   return GoRouter(
-    initialLocation: "/screen2",
+    initialLocation: "/screen1",
     routes: [
-      // todo:
-      // GoRoute(path: "/screen1", builder: (context, state) => Screen1()),
+      GoRoute(
+          path: "/screen1",
+          builder: (context, state) {
+            var vm = Screen1Vm(countRepo);
+            return Screen1(vm);
+          }),
       GoRoute(path: "/screen2", builder: (context, state) => Screen2())
     ],
   );
