4a5ca1c38db719ca791058beec5377fbcc94b6ad
siddhant <siddhantkumarupmanyu@gmail.com>
2023-02-16 15:33:31 +0530

    refactor InMemoryCountRepository_test

--- test/InMemoryCountRepository_test.dart
+++ test/InMemoryCountRepository_test.dart
@@ -3,18 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_hello_world/InMemoryCountRepository.dart';
 
 void main() {
-  test("savesCount", () async {
-    final repo = InMemoryCountRepository(20);
+  late InMemoryCountRepository repo;
+  late StreamQueue<int> streamQueue;
 
+  setUp(() {
+    repo = InMemoryCountRepository(20);
+    streamQueue = StreamQueue<int>(repo.countSteam);
+  });
+
+  test("getCount", () async {
     await repo.saveCount(10);
-
     expect(await repo.getCount(), equals(10));
   });
 
   test("emitsCount", () async {
-    final repo = InMemoryCountRepository(20);
-    final streamQueue = StreamQueue<int>(repo.countSteam);
-
     // i need to await so that saveCount finishes the operation without putting anything in the steam,
     // so streams onListen logic can be verified
     await repo.saveCount(5);
@@ -28,4 +30,7 @@ void main() {
     repo.saveCount(20);
     expect(await streamQueue.next, equals(20));
   });
+
+  // test("getAndUpdate", () async {
+  // });
 }
