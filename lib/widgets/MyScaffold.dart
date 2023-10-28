8ba8b1bcf827c79de9a3b26ae6b58e2c278316ff
siddhant <siddhantkumarupmanyu@gmail.com>
2023-10-28 12:55:23 +0530

    make MyScaffold generic so it can listen for different types

--- lib/widgets/MyScaffold.dart
+++ lib/widgets/MyScaffold.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class MyScaffold extends StatelessWidget {
+class MyScaffold<T extends MyScaffoldDto> extends StatelessWidget {
   const MyScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var myScaffoldDto = context.watch<MyScaffoldDto>();
+    var myScaffoldDto = context.watch<T>();
 
     return Scaffold(
       appBar: AppBar(title: Text(myScaffoldDto.appBarValue)),
@@ -21,10 +21,11 @@ class MyScaffold extends StatelessWidget {
   }
 }
 
-class MyScaffoldDto {
-  final String value;
-  final void Function() callback;
-  final String appBarValue;
-
-  const MyScaffoldDto(this.value, this.callback, this.appBarValue);
+// todo: upgrade
+// This requires the 'class-modifiers' language feature to be enabled.
+// Try updating your pubspec.yaml to set the minimum SDK constraint to 3.0.0 or higher, and running 'pub get
+abstract class MyScaffoldDto {
+  abstract final String value;
+  abstract final void Function() callback;
+  abstract final String appBarValue;
 }
--- test/widget/MyScaffold_test.dart
+++ test/widget/MyScaffold_test.dart
@@ -5,6 +5,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_hello_world/widgets/MyScaffold.dart';
 
+class TestScaffoldDto implements MyScaffoldDto {
+  TestScaffoldDto(this.appBarValue, this.callback, this.value);
+
+  @override
+  final String appBarValue;
+
+  @override
+  final void Function() callback;
+
+  @override
+  final String value;
+}
+
 void main() {
   Future<StreamController<MyScaffoldDto>> pumpWidgetUsing(
       WidgetTester widgetTester) async {
@@ -14,9 +27,11 @@ void main() {
     await widgetTester.pumpWidget(MaterialApp(
         home: StreamProvider<MyScaffoldDto>.value(
       value: streamController.stream,
-      initialData: MyScaffoldDto("100", () {}, "App Bar"),
+      initialData: TestScaffoldDto("100", () {}, "App Bar"),
       child: const MyScaffold(),
     )));
+    var foo = const MyScaffold();
+    print(foo); // Instance of 'Foo<SomeBaseClass>'
 
     return streamController;
   }
@@ -25,7 +40,7 @@ void main() {
     var streamController = await pumpWidgetUsing(widgetTester);
     expect(find.text("App Bar"), findsOneWidget);
     expect(find.text("100"), findsOneWidget);
-    streamController.add(MyScaffoldDto("20", () {}, "App Bar"));
+    streamController.add(TestScaffoldDto("20", () {}, "App Bar"));
     await widgetTester.pumpAndSettle();
     expect(find.text("20"), findsOneWidget);
   });
@@ -34,7 +49,7 @@ void main() {
     var streamController = await pumpWidgetUsing(widgetTester);
 
     var isCalled = false;
-    streamController.add(MyScaffoldDto("100", () {
+    streamController.add(TestScaffoldDto("100", () {
       isCalled = true;
     }, "App Bar"));
     await widgetTester.pumpAndSettle();
@@ -47,7 +62,7 @@ void main() {
   testWidgets("updatesAppBarText", (widgetTester) async {
     var streamController = await pumpWidgetUsing(widgetTester);
     expect(find.text("App Bar"), findsOneWidget);
-    streamController.add(MyScaffoldDto("20", () {}, "Updated App Bar"));
+    streamController.add(TestScaffoldDto("20", () {}, "Updated App Bar"));
     await widgetTester.pumpAndSettle();
     expect(find.text("Updated App Bar"), findsOneWidget);
   });
