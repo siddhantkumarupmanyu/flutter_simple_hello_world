3fc4b2d03255b0a94e8e69802faa7a83b03f35a6
siddhant <siddhantkumarupmanyu@gmail.com>
2023-10-29 14:40:44 +0530

    fix e2e_test.dart

--- integration_test/e2e_test.dart
+++ integration_test/e2e_test.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spike_state_management/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter_spike_state_management/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("navigatesToScreen2After10Clicks", (WidgetTester tester) async {
+  testWidgets("navigatesToScreen2After15Clicks", (WidgetTester tester) async {
     app.main();
     await tester.pumpAndSettle();
 
     expect(find.text("Screen 1"), findsOneWidget);
-    expect(find.text("0"), findsOneWidget);
+    expect(find.text("36"), findsOneWidget);
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 15; i++) {
       await tester.tap(find.byType(FloatingActionButton));
     }
 
+    await tester.pumpAndSettle();
+    await Future.delayed(const Duration(seconds: 8));
+    await tester.pumpAndSettle();
     expect(find.text("Screen 2"), findsOneWidget);
   });
 }
